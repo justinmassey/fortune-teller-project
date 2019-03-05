e961e072708f54409666c5a967f90fd0ac682f9c
justinmassey <43577144+justinmassey@users.noreply.github.com>
2019-03-05 09:31:02 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,7 @@
+$("button").click(function() {
+    var a=$("qwerty").val();
+    var b=$("qwerty").val();
+    var c=$("qwerty").val();
+    var weeksnum=parseInt(c);
+    $(".ptext").text(name + "," + " " + " by the year 2050, you will have eaten " + " " + food + " " weeksnum * 52 * 31 + " " + " times ");
+});
